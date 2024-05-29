3101367bc25d34eb0550beca9e9496c38f0d647c
Oghenemano Utomudo Omogha <manoomogha@yahoo.com>
2024-05-29 10:30:30 +0200

    change operator visitor class

--- src/de/uni_passau/fim/se2/sa/readability/utils/OperatorVisitor.java
+++ src/de/uni_passau/fim/se2/sa/readability/utils/OperatorVisitor.java
@@ -1,11 +1,9 @@
 package de.uni_passau.fim.se2.sa.readability.utils;
 
-import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.AssignExpr;
-import com.github.javaparser.ast.expr.UnaryExpr;
-import com.github.javaparser.ast.expr.ConditionalExpr;
-import com.github.javaparser.ast.expr.InstanceOfExpr;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,50 +14,56 @@ public class OperatorVisitor extends VoidVisitorAdapter<Void> {
         BINARY,             // x+y
         UNARY,              // -x, ++x
         CONDITIONAL,        // ==, <=, &&, ||
-        TYPE_COMPARISON    // instanceof
+        TYPE_COMPARISON,    // instanceof
     }
 
     private final Map<OperatorType, Integer> operatorsPerMethod;
 
     public OperatorVisitor() {
         operatorsPerMethod = new HashMap<>();
-        // Initialize the counts for each operator type
-        for (OperatorType type : OperatorType.values()) {
-            operatorsPerMethod.put(type, 0);
-        }
-    }
-
-    @Override
-    public void visit(BinaryExpr n, Void arg) {
-        super.visit(n, arg);
-        operatorsPerMethod.put(OperatorType.BINARY, operatorsPerMethod.get(OperatorType.BINARY) + 1);
-    }
-
-    @Override
-    public void visit(AssignExpr n, Void arg) {
-        super.visit(n, arg);
-        operatorsPerMethod.put(OperatorType.ASSIGNMENT, operatorsPerMethod.get(OperatorType.ASSIGNMENT) + 1);
-    }
-
-    @Override
-    public void visit(UnaryExpr n, Void arg) {
-        super.visit(n, arg);
-        operatorsPerMethod.put(OperatorType.UNARY, operatorsPerMethod.get(OperatorType.UNARY) + 1);
-    }
-
-    @Override
-    public void visit(ConditionalExpr n, Void arg) {
-        super.visit(n, arg);
-        operatorsPerMethod.put(OperatorType.CONDITIONAL, operatorsPerMethod.get(OperatorType.CONDITIONAL) + 1);
-    }
-
-    @Override
-    public void visit(InstanceOfExpr n, Void arg) {
-        super.visit(n, arg);
-        operatorsPerMethod.put(OperatorType.TYPE_COMPARISON, operatorsPerMethod.get(OperatorType.TYPE_COMPARISON) + 1);
     }
 
     public Map<OperatorType, Integer> getOperatorsPerMethod() {
         return operatorsPerMethod;
     }
+
+    @Override
+    public void visit(BinaryExpr n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.BINARY, operatorsPerMethod.getOrDefault(OperatorType.BINARY, 0) + 1);
+    }
+
+    @Override
+    public void visit(AssignExpr n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.ASSIGNMENT, operatorsPerMethod.getOrDefault(OperatorType.ASSIGNMENT, 0) + 1);
+    }
+
+    @Override
+    public void visit(VariableDeclarator n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.ASSIGNMENT, operatorsPerMethod.getOrDefault(OperatorType.ASSIGNMENT, 0) + 1);
+    }
+
+    @Override
+    public void visit(ConditionalExpr n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.CONDITIONAL, operatorsPerMethod.getOrDefault(OperatorType.CONDITIONAL, 0) + 1
+        );
+    }
+
+    @Override
+    public void visit(InstanceOfExpr n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.TYPE_COMPARISON, operatorsPerMethod.getOrDefault(OperatorType.TYPE_COMPARISON, 0) + 1);
+    }
+
+    @Override
+    public void visit(UnaryExpr n, Void arg) {
+        super.visit(n, arg);
+        operatorsPerMethod.put(OperatorType.UNARY, operatorsPerMethod.getOrDefault(OperatorType.UNARY, 0) + 1);
+    }
+ 
+
+
 }
